c00fccee858f60575a40440a644003fc0cfd6f51
ChuzaWick420 <zaiddadev@gmail.com>
2024-02-03 13:10:29 +0500

    Added: selection options in the side panel for layout

--- src/pages/layout/layout.jsx
+++ src/pages/layout/layout.jsx
@@ -3,11 +3,24 @@ import React from "react";
 import "./layout.css";
 
 function Layout() {
+    let Options = ((props)=>{
+        return (
+            <div className="nav_button">
+                <img src="/placeholder.png" />
+                <h1>{props.content}</h1>
+            </div>
+        );
+    });
+
     return (
         <div className="layout_container">
             <div className="img_mid_container">
                 <img src="/Artorias.png" />
             </div>
+            <Options content="Overview" />
+            <Options content="Experience" />
+            <Options content="Projects" />
+            <Options content="Case Studies" />
         </div>
     );
 }
