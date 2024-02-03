1790e7be569b9d98fb65b13d57f7036f4036f8b1
ChuzaWick420 <zaiddadev@gmail.com>
2024-02-03 11:27:12 +0500

    Added: Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={}>
+          <Route path="overview" index element={} />
+          <Route path="experience" element={} />
+          <Route path="projects" element={} />
+          <Route path="case_studies" element={} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
