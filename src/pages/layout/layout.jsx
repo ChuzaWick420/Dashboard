9b1544386fb3b35d59471fc7c20bcdaf789ae98f
ChuzaWick420 <zaiddadev@gmail.com>
2024-02-05 20:51:07 +0500

    Added: mobile layout using media queries
    only added layout change for now

--- src/pages/layout/layout.jsx
+++ src/pages/layout/layout.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./layout.css";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
+    
+    const [targetWidth, setTargetWidth] = useState(window.innerWidth);
+ 
     let Options = ((props)=>{
         return (
             <div className="nav_button">
@@ -13,19 +16,67 @@ function Layout() {
         );
     });
 
+    //break points for large phone, tablet, laptop
+    const breakPoints = [425, 768, 1024];
+
+    useEffect(()=>{
+        window.addEventListener("resize", ()=>{
+            for (let i = 1; i < breakPoints.length; i++) {
+                let average = (breakPoints[i] + breakPoints[i - 1]) / 2;
+
+                if (window.innerWidth < average) {
+                    setTargetWidth(breakPoints[i - 1]);
+                    break;
+                }
+                else {
+                    setTargetWidth(breakPoints[i]);
+
+                    if(window.innerWidth < breakPoints[i])
+                        break;
+                }
+            }
+            
+        });
+    }, []);
+
+    let Comp = ()=>{
+        switch (targetWidth) {
+        
+            case breakPoints[0]:
+                return (
+                    <>
+                        <h1>PlaceHolder</h1>
+                        <div className="burger">
+                            <div className="burger_line_1"></div>
+                            <div className="burger_line_2"></div>
+                            <div className="burger_line_3"></div>
+                        </div>
+                    </>
+                );
+            
+            default:
+                return (
+                    <>
+                        <Options content="Overview" />
+                        <Options content="Experience" />
+                        <Options content="Projects" />
+                        <Options content="Case Studies" />
+                    </>
+                );
+        }
+    }
+
     return (
-        <>
-            <div className="layout_container">
+        <div className="layout_container">
+            <div className="side_panel">
                 <div className="img_mid_container">
                     <img src="/Artorias.png" />
                 </div>
-                <Options content="Overview" />
-                <Options content="Experience" />
-                <Options content="Projects" />
-                <Options content="Case Studies" />
+            <Comp />
             </div>
-            <Outlet /> 
-        </>
+            {/* <Outlet />  */}
+            <div className="dev_container">test</div>
+        </div>
     );
 }
 
