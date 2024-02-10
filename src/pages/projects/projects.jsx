04a95708bd96304ebb1c066262d65166a84bb0fb
ChuzaWick420 <zaiddadev@gmail.com>
2024-02-10 10:35:01 +0500

    Added: projects card

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,8 +1,48 @@
 import React from "react";
 
+import "./projects.css";
+
 function Projects() {
+
+    let ProjectCard = ()=>{
+        return (
+            <div className="project_card">
+                <div className="project_header">
+                    <div className="project_details">
+                        <h3 className="important_text">Project_name</h3>
+                        <h4>Customer: @this-guy</h4>
+                        <h4>Started at: 1-1-2024</h4>
+                    </div>
+                    <div className="project_img">
+                        <img src="/placeholder.png" />
+                    </div>
+                </div>
+                <div className="project_content">
+                    <div className="projects_content_section">
+                        <h4>Description Heading</h4>
+                        <p>content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content </p>
+                        <h4>Description Heading</h4>
+                        <p>content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content </p>
+                        <h4>Description Heading</h4>
+                        <p>content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content </p>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
     return (
-        <div className="projects_container"></div>
+        <div className="projects_container">
+            <div className="projects_list">
+                <div className="category_container">
+                    <h1 className="important_text">Company_name</h1>
+                    <ProjectCard />
+                </div>
+            </div>
+            <div className="nav_table">
+
+            </div>
+        </div>
     );
 }
 
