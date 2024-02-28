a2dd70db3f91d1eaa1f5e6226f87d6ff065443f8
ChuzaWick420 <zaiddadev@gmail.com>
2024-02-28 16:39:52 +0500

    Added template for the cast_studies pages

--- src/pages/case_studies/case_studies.jsx
+++ src/pages/case_studies/case_studies.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import "./case_studies.css";
+
+function ProjectContainer (props) {
+
+    const [section, setSection] = useState([]);
+
+    useEffect(()=>{
+        let paragraphs = [];
+
+        for (let paragraph of props.content) {
+            paragraphs.push(
+                <div className="study_section" key={props.content.indexOf(paragraph)}>
+                    <h4>{paragraph[0]}</h4>
+                    <p>{paragraph[1]}</p>
+                </div>
+            );
+        }
+
+        setSection(paragraphs);
+
+    }, []);
+
+    return (
+        <div className="category_container">
+            <h1 className="important_text">{props.proj}</h1>
+            <div className="study_details">
+                {section}
+            </div>
+        </div>
+    );
+};
 
 function Case_Studies() {
+
+
     return (
-        <div className="case_studies"></div>
+        <div className="case_studies_container">
+            <ProjectContainer proj="Project_1" content={[
+                [
+                    "Heading_1",
+                    "study_details_paragraph"
+                ],
+                [
+                    "Heading_2",
+                    "study_details_paragraph"
+                ]
+            ]} />
+        </div>
     );
 }
 
