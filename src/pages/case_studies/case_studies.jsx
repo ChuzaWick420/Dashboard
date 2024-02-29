6d2067f278659a39791e112046728b6597f6cbfd
ChuzaWick420 <zaiddadev@gmail.com>
2024-02-29 10:25:04 +0500

    Added: nav section to the page

--- src/pages/case_studies/case_studies.jsx
+++ src/pages/case_studies/case_studies.jsx
@@ -26,27 +26,123 @@ function ProjectContainer (props) {
         <div className="category_container">
             <h1 className="important_text">{props.proj}</h1>
             <div className="study_details">
-                {section}
+                <div>
+                    {section}
+                </div>
             </div>
         </div>
     );
 };
 
+function StudyNav (props) {
+    
+    const[projects, setProjects] = useState([]);
+
+    useEffect(()=>{
+        let projects_list = [];
+
+        for (let project of props.project_names) {
+            projects_list.push(
+                <Project key={props.project_names.indexOf(project)} heading={project} sections={props.meta_gen(props.project_names.indexOf(project))} />
+            );
+        }
+
+        setProjects(projects_list);
+
+    }, []);
+
+    const Project = (props) => {
+        
+        const [sections, setSections] = useState([]);
+        
+        useEffect(()=>{
+            let sections_list = [];
+
+            for (let section of props.sections) {
+                sections_list.push(
+                    <p key={props.sections.indexOf(section)} className="study_project_section">{section}</p>
+                );
+            }
+
+            setSections(sections_list);
+
+        }, []);
+
+        return (
+            <div className="study_project">
+                <p className="study_project_heading">{props.heading}</p>
+                {sections}
+            </div>
+        );
+    }
+    
+    return (
+        <div className="study_nav">
+            {projects}
+        </div>
+    );
+}
+
 function Case_Studies() {
 
+    const studies_meta = [
+        {
+            "Project_1": [
+                "Heading_1_P1",
+                "Heading_2_P1",
+                "Heading_3_P1"
+            ]
+        },
+        {
+            "Project_2": [
+                "Heading_1_P2",
+                "Heading_2_P2",
+                "Heading_3_P2"
+            ]
+        }
+    ];
+
+    const projectNames = studies_meta.map((obj)=>{
+        return Object.keys(obj)[0];
+    });
+
+    function getProjectHeadings (proj_index) {
+        return studies_meta[proj_index][projectNames[proj_index]];
+    }
 
     return (
         <div className="case_studies_container">
-            <ProjectContainer proj="Project_1" content={[
-                [
-                    "Heading_1",
-                    "study_details_paragraph"
-                ],
-                [
-                    "Heading_2",
-                    "study_details_paragraph"
-                ]
-            ]} />
+            <div className="study_list">
+                <ProjectContainer proj={projectNames[0]} content={[
+                    [
+                        getProjectHeadings(0)[0],
+                        "Paragraph"                   
+                    ],
+                    [
+                        getProjectHeadings(0)[1],
+                        "Paragraph"
+                    ],
+                    [
+                        getProjectHeadings(0)[2],
+                        "Paragraph"
+                    ]
+                ]} />
+                <ProjectContainer proj={projectNames[1]} content={[
+                    [
+                        getProjectHeadings(1)[0],
+                        "study_details_paragraph "
+                    ],
+                    [
+                        getProjectHeadings(1)[1],
+                        "study_details_paragraph"
+                    ],
+                    [
+                        getProjectHeadings(1)[2],
+                        "study_details_paragraph"
+                    ]
+                ]} />
+            </div>
+            <StudyNav project_names={projectNames} meta_gen={getProjectHeadings} />
         </div>
     );
 }
