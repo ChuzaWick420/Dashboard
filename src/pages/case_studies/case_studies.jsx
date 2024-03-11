2ab819b078cac385892214877ac3fae8a9becaff
ChuzaWick420 <zaiddadev@gmail.com>
2024-03-11 18:08:59 +0500

    Refactoed case_studies page code and implemented navigation to certain parts
    Bug: the page on mobile view scrolls and gets rid of the mobile nav bar

--- src/pages/case_studies/case_studies.jsx
+++ src/pages/case_studies/case_studies.jsx
@@ -3,29 +3,32 @@ import React, { useContext, useEffect, useState } from "react";
 import "./case_studies.css";
 import { windowSize } from "../layout/layout";
 
+import data from "../../json/case_studies.json";
+
 function ProjectContainer (props) {
 
     const [section, setSection] = useState([]);
 
     useEffect(()=>{
-        let paragraphs = [];
 
-        for (let paragraph of props.content) {
-            paragraphs.push(
-                <div className="study_section" key={props.content.indexOf(paragraph)}>
-                    <h4>{paragraph[0]}</h4>
-                    <p>{paragraph[1]}</p>
-                </div>
-            );
-        }
-
-        setSection(paragraphs);
+        setSection(
+            props.study.sections.map((section, index)=>{
+                return (
+                    <div className="study_section" id={section.id} key={index}>
+                        <h4>{section.heading}</h4>
+                        {section.paragraphs.map((para, index)=>{
+                            return <p key={index}>{para}</p>
+                        })}
+                    </div>
+                );
+            })
+        );
 
     }, []);
 
     return (
-        <div className="category_container">
-            <h1 className="important_text">{props.proj}</h1>
+        <div id={props.study.id} className="category_container">
+            <h1 className="important_text">{props.study.name}</h1>
             <div className="study_details">
                 <div>
                     {section}
@@ -95,69 +98,54 @@ function MobileNav (props) {
     }
 }
 
+const Project = (props) => {
+
+    const [sections, setSections] = useState([]);
+
+    useEffect(()=>{
+        
+        setSections(
+            props.sections.map((section, index)=>{
+                return (
+                    <a href={`#${section.id}`} key={index} >
+                        <p key={index} className="study_project_section">{section.heading}</p>
+                    </a>
+                );
+            })
+        );
+
+    }, []);
+
+    return (
+        <div className="study_project">
+            <a href={`#${props.study_id}`}>
+                <p className="study_project_heading">{props.heading}</p>
+            </a>
+            {sections}
+        </div>
+    );
+
+}
+
 function Case_Studies() {
 
     const [navClass, setNavClass] = useState("study_nav");
-
-    const studies_meta = [
-        {
-            "Project_1": [
-                "Heading_1_P1",
-                "Heading_2_P1",
-                "Heading_3_P1"
-            ]
-        },
-        {
-            "Project_2": [
-                "Heading_1_P2",
-                "Heading_2_P2",
-                "Heading_3_P2"
-            ]
-        }
-    ];
 
     function StudyNav (props) {
 
         const[projects, setProjects] = useState([]);
 
         useEffect(()=>{
-            let projects_list = [];
 
-            for (let project of props.project_names) {
-                projects_list.push(
-                    <Project key={props.project_names.indexOf(project)} heading={project} sections={props.meta_gen(props.project_names.indexOf(project))} />
-                );
-            }
-
-            setProjects(projects_list);
-
-        }, []);
-
-        const Project = (props) => {
-
-            const [sections, setSections] = useState([]);
-
-            useEffect(()=>{
-                let sections_list = [];
-
-                for (let section of props.sections) {
-                    sections_list.push(
-                        <p key={props.sections.indexOf(section)} className="study_project_section">{section}</p>
+            setProjects(
+                props.study_data.map((study, index)=>{
+                    return (
+                        <Project key={index} heading={study.name} sections={study.sections} study_id={study.id} />
                     );
-                }
-
-                setSections(sections_list);
-
-            }, []);
-
-            return (
-                <div className="study_project">
-                    <p className="study_project_heading">{props.heading}</p>
-                    {sections}
-                </div>
+                })
             );
 
-        }
+        }, []);
     
         return (
             <div className={props.active_state}>
@@ -167,56 +155,26 @@ function Case_Studies() {
 
     }
 
-    const projectNames = studies_meta.map((obj)=>{
-        return Object.keys(obj)[0];
+    const projectNames = data["studies_meta"].map((study)=>{
+        return Object.keys(study)[0];
     });
-
-    function getProjectHeadings (proj_index) {
-        return studies_meta[proj_index][projectNames[proj_index]];
-    }
 
     return (
         <div className="case_studies_container">
  
             <MobileNav 
-                projects={projectNames}
-                meta_gen={getProjectHeadings}
                 nav_controller={{
                     "state": navClass,
                     "handler": setNavClass
             }} />
 
             <div className="study_list">
-                <ProjectContainer proj={projectNames[0]} content={[
-                    [
-                        getProjectHeadings(0)[0],
-                        "Paragraph"                   
-                    ],
-                    [
-                        getProjectHeadings(0)[1],
-                        "Paragraph"
-                    ],
-                    [
-                        getProjectHeadings(0)[2],
-                        "Paragraph"
-                    ]
-                ]} />
-                <ProjectContainer proj={projectNames[1]} content={[
-                    [
-                        getProjectHeadings(1)[0],
-                        "study_details_paragraph "
-                    ],
-                    [
-                        getProjectHeadings(1)[1],
-                        "study_details_paragraph"
-                    ],
-                    [
-                        getProjectHeadings(1)[2],
-                        "study_details_paragraph"
-                    ]
-                ]} />
+                {data["studies_meta"].map((study, index)=>{
+                    return <ProjectContainer key={index} study={study} />
+                })}
             </div>
-            <StudyNav project_names={projectNames} meta_gen={getProjectHeadings} active_state={navClass} />
+
+            <StudyNav project_names={projectNames} study_data={data["studies_meta"]} active_state={navClass} />
         </div>
     );
 }
